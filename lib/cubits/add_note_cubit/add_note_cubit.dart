8d94e04ec32597d26e1c5954e6e27fd1d9f0e24c
Fatma Gelil <93225388+fatma-gelil@users.noreply.github.com>
2025-03-25 16:35:32 +0200

    register adapter

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -2,7 +2,10 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
+import 'package:notes_app/models/note_model.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+  addNote(NoteModel note){
+      }
 }
--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -3,3 +3,13 @@
 sealed class AddNotesState {}
 
 final class AddNotesInitial extends AddNotesState {}
+final class AddNotesLoading extends AddNotesState {}
+final class AddNotesSuccess extends AddNotesState {
+
+}
+final class AddNotesFailure extends AddNotesState {
+  final String errorMessage;
+  AddNotesFailure(this.errorMessage); 
+  }
+
+ 
