e67cd5db21c05fee6165d285e37dc16daf44f354
Fatma Gelil <93225388+fatma-gelil@users.noreply.github.com>
2025-03-26 06:36:42 +0200

    feat: enhance add note functionality with loading state and button feedback

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
@@ -31,9 +30,13 @@ class _AddNoteButtomSheetState extends State<AddNoteButtomSheet> {
             }
           },
           builder: (context, state) {
-            return ModalProgressHUD(
-                inAsyncCall: state is AddNotesLoading ? true : false,
-                child: SingleChildScrollView(child: AddNoteForm()));
+            return AbsorbPointer(
+              absorbing: state is AddNotesLoading ? true : false,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SingleChildScrollView(child: AddNoteForm()),
+              ),
+            );
           },
         ),
       ),
--- lib/views/widgets/custom_add_button.dart
+++ lib/views/widgets/custom_add_button.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomAddButton extends StatelessWidget {
-  const CustomAddButton({super.key, required this.text, this.ontap});
+  const CustomAddButton(
+      {super.key, required this.text, this.ontap, this.isLoading = false});
   final String text;
   final void Function()? ontap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ontap ,
+      onTap: ontap,
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 55,
@@ -17,11 +19,21 @@ class CustomAddButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
         ),
         child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
+          child: isLoading
+              ? SizedBox(
+                width: 24,
+                height: 24,
+                child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+              )
+              : Text(
+                  text,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
         ),
       ),
     );
