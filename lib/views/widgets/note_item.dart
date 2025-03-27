297dcca9cb830fa5119a67cb62be04acc5b420f6
Fatma Gelil <93225388+fatma-gelil@users.noreply.github.com>
2025-03-27 23:26:55 +0200

    add delete functionality to note item

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -38,7 +38,9 @@ class NoteItem extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    noteModel.delete();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     size: 24,
